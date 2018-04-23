d15bc4bdc5dac6021fba7e39b4c876a4f059f3d0
knappek <andreas.knapp@accenture.com>
2018-04-22 22:04:53 -0700

    search for ExternalIP in nodes status if ExternalDNS is not available

--- ssh.sh
+++ ssh.sh
@@ -14,5 +14,11 @@ else
     path="{.items[0].status.addresses[?(.type=='ExternalDNS')].address}"
   fi
   hostname=$(kubectl get node $1 -o jsonpath="${path}" $selector)
-  ssh -i ${KUBECTL_PLUGINS_LOCAL_FLAG_IDENTITY_FILE} ${KUBECTL_PLUGINS_LOCAL_FLAG_SSH_USER}@$hostname
+  [[ -z $hostname ]] && hostname=$(kubectl get node $1 -o jsonpath="${path/ExternalDNS/ExternalIP}" $selector)
+  if [[ ! -z $hostname  ]]; then
+    ssh -i ${KUBECTL_PLUGINS_LOCAL_FLAG_IDENTITY_FILE} ${KUBECTL_PLUGINS_LOCAL_FLAG_SSH_USER}@$hostname
+  else
+    echo "Can't find Public DNS/IP"
+    exit 1
+  fi
 fi
